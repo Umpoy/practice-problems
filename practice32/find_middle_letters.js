b44d14dbd09ad002503023608ebc2806e214c87d
Umpoy <iumpoy@yahoo.com>
2017-12-10 18:29:55 -0800

    practice 32 completed

--- practice32/find_middle_letters.js
+++ practice32/find_middle_letters.js
@@ -1,5 +1,13 @@
 const find_middle_letters = string => {
-    if(string.length % 2 == 0){
-        return string.substr()
+    if (string.length % 2 == 0) {
+        return string.charAt((string.length / 2) - 1) + string.charAt(string.length / 2)
+    } else {
+        return string.charAt(Math.round(string.length / 2))
     }
 }
+
+console.log(find_middle_letters('dish'));
+console.log(find_middle_letters('cat'));
+console.log(find_middle_letters('wambo'));
+console.log(find_middle_letters('tacocat'));
+console.log(find_middle_letters('watercat'));
